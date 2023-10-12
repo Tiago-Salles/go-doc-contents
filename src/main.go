4452eab966d080ab8bdb67da718ef72d45832575
Tiago-Salles <tiagosalles20@gmail.com>
2023-10-12 23:39:25 +0000

    feat: added slices code

--- src/main.go
+++ src/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Tiago-Salles/go-doc-contents/src/datastruct"
 )
 
 func main() {
 	datastruct.Arrays()
-	fmt.Println("Hello, world.")
+	datastruct.Slices()
 }
